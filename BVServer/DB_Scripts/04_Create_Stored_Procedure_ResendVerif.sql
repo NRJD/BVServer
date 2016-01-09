787327e9b218ac6cc89541e750c9275fc3315b3c
Sathya <kinnu_sathya@yahoo.com~>
2016-01-09 16:25:53 -0600

    Added a new procedure for Resend Verification

---
+++ BVServer/DB_Scripts/04_Create_Stored_Procedure_ResendVerif.sql
@@ -0,0 +1,44 @@
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_updateVerifCodes`(
+
+				IN emailId varchar(50) 
+				,IN emailVerifCode varchar(32) 
+                ,IN mobVerifCode int(6) )
+BEGIN
+
+declare	emailInDB varchar(50);
+declare	acctVerified int(1);
+declare statusCode int(4);
+
+set @acctVerified = 0;
+
+	SELECT ACCT_VERIFIED into @acctVerified FROM bv.user_login where email_id  = emailId;
+    
+    IF @acctVerified = 0 THEN
+    
+		BEGIN
+			
+            UPDATE 
+				user_login 
+			SET 
+				EMAIL_VERIF_CODE = emailVerifCode,
+				MOBILE_VERIF_CODE = mobVerifCode  
+			WHERE 
+				EMAIL_ID=emailId;
+                
+			SET @statusCode = 3033 ; -- Reset Codes success
+            
+		END;
+        
+	ELSE
+    
+		SET @statusCode = 3011 ; -- Account Already Verified
+	
+    END IF;
+    
+    SELECT @statusCode as 'STATUS_FROM_DB';
+
+END$$
+DELIMITER ;
+
